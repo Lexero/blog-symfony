21e8966a4a30929e05acec7990fb01d9506f383c
Ivanov Aleksandr <33204894+Lexero@users.noreply.github.com>
2022-12-14 14:43:07 +0300

    Added fixtures for blog posts

--- tests/DataFixtures/AppFixtures.php
+++ tests/DataFixtures/AppFixtures.php
@@ -1,17 +1,42 @@
 <?php
 
-namespace App\DataFixtures;
+namespace App\Tests\DataFixtures;
 
+use App\Entity\BlogPost;
+use Cocur\Slugify\SlugifyInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    private \Faker\Generator $faker;
 
+    private SlugifyInterface $slug;
+
+    public function __construct(SlugifyInterface $slugify)
+    {
+        $this->faker = Factory::create();
+        $this->slug = $slugify;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $this->loadPost($manager);
+    }
+
+    public function loadPost(ObjectManager $manager)
+    {
+        for ($i = 1; $i < 20; $i++) {
+            $post = new BlogPost();
+            $post->setTitle($this->faker->text(100));
+            $post->setSlug($this->slug->slugify($post->getTitle()));
+            $post->setBody($this->faker->text(1000));
+            $post->setCreatedAt($this->faker->dateTime);
+            $post->setAuthor('Alex');
+
+            $manager->persist($post);
+        }
         $manager->flush();
     }
 }
