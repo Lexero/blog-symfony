cb5a4566bccf5f727456cc3296d662fb26c11032
Ivanov Aleksandr <ivanov181097@mail.ru>
2023-02-15 20:06:41 +0300

    added locks in admin panel

--- src/Controller/AdminCRUDController.php
+++ src/Controller/AdminCRUDController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Service\Locker;
 use Sonata\AdminBundle\Controller\CRUDController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +11,30 @@ use Symfony\Contracts\Service\ServiceSubscriberInterface;
 
 class AdminCRUDController extends CRUDController implements ServiceSubscriberInterface
 {
+    private Locker $locker;
+
+    public function __construct(Locker $locker)
+    {
+        $this->locker = $locker;
+    }
+
     public function editAction(Request $request): Response
     {
-        try {
-            return parent::editAction($request);
-        } catch (\RuntimeException $exception) {
-            $this->addFlash('error', $exception->getMessage());
-            return $this->redirect("/admin/app/blogpost/list");
+        $authorizedUser = $this->getUser();
+        if ($authorizedUser instanceof User) {
+            $key = $request->getRequestUri();
+            $userId = $this->locker->getLockedBy($key);
+            if ($userId !== null) {
+                if ($authorizedUser->getId() === $userId) {
+                    $this->locker->refreshLock($key, $userId);
+                } else {
+                    $this->addFlash('error', sprintf("User with ID %d working with that post", $userId));
+                    return $this->listAction($request);
+                }
+            } else {
+                $this->locker->setLock($key, $authorizedUser->getId());
+            }
         }
+        return parent::editAction($request);
     }
 }
