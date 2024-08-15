042f6900e43abc94630dc7f65e8beea869c5b3ff
Aleksandr <ivanov1810@mail.ru>
2024-08-15 17:08:31 +0300

    fix DATABASE_URL

--- src/Command/UserManager.php
+++ src/Command/UserManager.php
@@ -7,9 +7,9 @@ namespace App\Command;
 use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
 
-readonly class UserManager
+class UserManager
 {
-    public function __construct(private EntityManagerInterface $em)
+    public function __construct(private readonly EntityManagerInterface $em)
     {
     }
 
