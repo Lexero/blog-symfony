7c02c9b64277e64113211c9b979244538aac4f9d
Aleksandr <ivanov1810@mail.ru>
2024-08-16 13:23:22 +0300

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,11 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+//set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+//    if (E_DEPRECATED === $errno) {
+//        return true;
+//    }
+//
+//    return false;
+//});
